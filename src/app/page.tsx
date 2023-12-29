1ee52725de95cc982034313961076e8e4f2b1782
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-29 02:52:56 +0000

    ref: use smaller sizes for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="favicon.svg"
         alt="Adil's profile picture"
-        width={500}
-        height={500}
+        width={132}
+        height={140}
         className='mock-mac-os-icon'
       />,
       header: 'About me',
@@ -46,8 +46,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="https://raw.githubusercontent.com/saber-notes/saber/main/assets/icon/icon_macos.png"
         alt="Saber"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
       />,
       header: 'Saber',
       technologies: [Technology.flutter],
@@ -59,8 +59,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="https://raw.githubusercontent.com/adil192/ricochlime/main/assets/icon/icon_macos.png"
         alt="Ricochlime"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
       />,
       header: 'Ricochlime',
       technologies: [Technology.flutter],
@@ -72,8 +72,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/timing.svg"
         alt="Timing Flutter"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Timing Trainer',
@@ -86,8 +86,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/BlackboardTheme.webp"
         alt="Blackboard Theme"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Blackboard Theme',
@@ -100,8 +100,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/nonogram.webp"
         alt="Nonogram"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Nonogram',
@@ -115,8 +115,8 @@ export default async function AsyncHome() {
         // TODO(adil192): Make a better icon
         src="projects/blank.svg"
         alt="Social"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Social',
@@ -129,8 +129,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/quintle.png"
         alt="Quintle"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Quintle',
@@ -143,8 +143,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/spencover.webp"
         alt="S Pen"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'S Pen',
@@ -157,8 +157,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/fabrik.webp"
         alt="Fabrik"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Fabrik',
@@ -171,8 +171,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/clocks.webp"
         alt="Clocks"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Clocks',
@@ -185,8 +185,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/zombies.webp"
         alt="Zombies"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Zombies',
@@ -199,8 +199,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/calculator.webp"
         alt="Calculator"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Calculator',
@@ -213,8 +213,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/loffice-365.webp"
         alt="loffice-365"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'loffice-365',
@@ -227,8 +227,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/SamsungAppsPatcher.webp"
         alt="Samsung Apps Patcher"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Samsung Apps Patcher',
@@ -241,8 +241,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/S20Debloat.webp"
         alt="S20 Debloat"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'S20 Debloat',
@@ -255,8 +255,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/collatz.webp"
         alt="Collatz"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Collatz',
@@ -269,8 +269,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/colours.webp"
         alt="Colours"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Colours',
@@ -283,8 +283,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/repo.webp"
         alt="Repo"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'Repo',
@@ -297,8 +297,8 @@ export default async function AsyncHome() {
       icon: <Image
         src="projects/LIFE.webp"
         alt="LIFE"
-        width={1024}
-        height={1024}
+        width={80}
+        height={80}
         className='mock-mac-os-icon'
       />,
       header: 'LIFE',
