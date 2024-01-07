e0fe5a705f5c1a305baf180b5810e85b956dcfcd
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-01-07 11:49:55 +0000

    fix: inline markdown

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,16 @@ export default function AsyncHome() {
       />,
       header: 'About me',
       technologies: [],
-      content: __readMd('about.md'),
+      content: `
+I'm a software engineer and undergrad at the University of Manchester.
+
+Currently, I'm mostly working on a host of architectural improvements to [Saber](#saber).
+
+Find me on
+[GitHub](https://github.com/adil192)
+or
+[LinkedIn](https://www.linkedin.com/in/adil-hanney/).
+`,
       date: undefined,
     },
     {
@@ -35,7 +44,18 @@ export default function AsyncHome() {
       />,
       header: 'Saber',
       technologies: [Technology.flutter],
-      content: __readMd('saber.md'),
+      content: `
+Saber is the notes app built for handwriting. It was my first non-trivial Flutter project, and I've been working on it since July 2022.
+
+I'd been writing digital notes since 2020, and the only app that suited my needs was [Samsung Notes](https://www.samsung.com/uk/apps/samsung-notes/).
+Unfortunately, it was only compatible with Samsung's Android devices and select Windows devices. Additionally, it didn't work on Samsung's Android devices with custom firmware, such as LineageOS, which I was using at the time on my Samsung phone. I wanted to be able to access my notes on all my devices, regardless of the operating system or manufacturer.
+
+I'd been wanting to make my own notes app for a while and in the summer holidays of 2022, I restumbled upon Flutter and finally decided to give it a go. Nowadays, Saber has likely* tens of thousands of users, and it's advancing at a rapid pace. (*I don't directly track users, but some partial data is available from various sources.)
+
+Developing Saber was (and is continuing to be) a huge learning experience for me. I've successfully deployed custom GLSL shaders, continual performance optimisations, CI/CD workflows on GitHub Actions, backwards compatibility for new features, [adaptive and responsive UIs](https://docs.flutter.dev/ui/layout/responsive/adaptive-responsive#the-difference-between-an-adaptive-and-a-responsive-app), [reusable packages](https://pub.dev/packages?q=publisher%3Aadil.hanney.org+show%3Aunlisted), and much more.
+
+Saber is available on all major platforms; find out more at [saber.adil.hanney.org](https://saber.adil.hanney.org) or on [GitHub](https://github.com/saber-notes/saber).
+`,
       date: 'July 2022',
       images: [
         <img
@@ -75,7 +95,24 @@ export default function AsyncHome() {
       />,
       header: 'Ricochlime',
       technologies: [Technology.flutter],
-      content: __readMd('ricochlime.md'),
+      content: `
+Ricochlime is a satisfying ~~slime~~ monster shooter game.
+The name was originally a portmanteau of "ricochet" and "slime",
+but I've since had to switch out the graphics for licensing reasons
+so there aren't any slimes anymore! I developed Ricochlime to experiment
+with making a game in Flutter using the Flame game engine.
+
+I designed this to be a simple, easy, satisfying game to play.
+I often like to have something to do with my hands while I'm focusing on something else,
+and I find that games like this are perfect for that.
+The music is disabled by default, and
+the only ads in the app are a banner at the bottom of the screen,
+which I find is a good compromise between monetisation and user experience.
+
+Ricochlime is available on all major platforms and
+[online](https://ricochlime.adil.hanney.org).
+View more on [GitHub](https://github.com/adil192/ricochlime).
+`,
       date: 'August 2023'
     },
     {
@@ -89,7 +126,19 @@ export default function AsyncHome() {
       />,
       header: 'Timing Trainer',
       technologies: [Technology.flutter, Technology.pwa],
-      content: __readMd('timing.md'),
+      content: `
+Timing Trainer presents you with a blinking square and you guess the time between blinks.
+As you play, you'll get better at recognizing sub-second intervals.
+
+I first created this game/trainer as a PWA (progressive web app)
+[here](https://adil.hanney.org/timing/).
+Later, since it was a very simple app,
+I made a cross-platform version as my first foray into Flutter.
+
+The Flutter version is available on all major platforms and
+[online](https://adil.hanney.org/timing_flutter/).
+View more on [GitHub](https://github.com/adil192/timing_flutter).
+`,
       date: 'June 2022'
     },
     {
@@ -103,7 +152,26 @@ export default function AsyncHome() {
       />,
       header: 'Blackboard Theme',
       technologies: [Technology.scss, Technology.js],
-      content: __readMd('BlackboardTheme.md'),
+      content: `
+My \`BlackboardTheme\` project encompasses a collection of (unofficial) tweaks and improvements to the University of Manchester's websites including Blackboard. It does this with the browser extensions [Stylus](https://addons.mozilla.org/en-GB/firefox/addon/styl-us/) for styles and [Tampermonkey](https://addons.mozilla.org/en-GB/firefox/addon/tampermonkey/) for scripts, which inject my custom CSS and JavaScript into the pages.
+
+This project was originally only intended to apply Google's Material Design to my university's Blackboard website,
+but as it grew, I began to theme more of my university's websites and then started adding small scripts to add features or boost usability.
+Every time I encountered an issue, or saw something that I found difficult to use, I would try to fix it.
+
+One of the most improved areas is the video player. The course content videos were difficult to use as you had to use the mouse to click on the buttons to play/pause, click somewhere on the razor-thin progress bar to rewind, and go through a series of popups and dialogs to configure captions at the start of every video!
+I emailed the university about this, but didn't get a response, so I decided to fix it myself.
+I added the standard keyboard shortcuts for play/pause, rewind/fast-forward, volume control, captions, and fullscreen.
+I also set sensible defaults, including a larger font size, better fonts, and a smaller width so your eyes don't have to move as far to read the captions.
+I also changed the upscaling algorithm to favour sharpness over smoothness, as the fuzziness of lower quality videos was harder to read.
+
+Another notable area is the course selection screen. The default layout was a compacted list of courses with very long names, which made it difficult to find the course you wanted.
+I added a grid layout with course cards which stay in the same position over time to help you remember where your courses are.
+I assigned each of my modules and departments an image which is displayed on the card, and left instructions on how to change the image for your own courses.
+I also added a feature that dims courses from semester 1 when you're in semester 2, to help you focus on the courses you're currently studying.
+
+It's hard to describe all the changes I've made, so I recommend you check out the [GitHub repository](https://github.com/adil192/BlackboardTheme) which has a more complete list of features, before and after screenshots, and instructions on how to install it.
+`,
       date: 'May 2022',
       images: [
         <img
--- src/sections/about.md
+++
@@ -1,8 +0,0 @@
-I'm a software engineer and undergrad at the University of Manchester.
-
-Currently, I'm mostly working on a host of architectural improvements to [Saber](#saber).
-
-Find me on
-[GitHub](https://github.com/adil192)
-or
-[LinkedIn](https://www.linkedin.com/in/adil-hanney/).
